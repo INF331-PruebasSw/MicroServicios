f12b0c153678b51554935ba264b68ff57e90a890
laura-leiva <laura.leiva.cataldo@gmail.com>
2025-09-07 23:00:17 -0300

    prueba

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.secret_key = "mi_clave_secreta"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///microeventos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Rutas
-@app.route('/')
-def index():
-    return render_template('index.html')
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'  # redirige a login si no está logueado
+
+from routes import *
 
 if __name__ == "__main__":
     app.run(debug=True)
